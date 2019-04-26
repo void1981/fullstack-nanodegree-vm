aa5838647f1bbb262e7f80d78b982c8799faca8e
void1981 <void1950@gmail.com>
2019-04-26 18:08:23 +0200

    create classes and mapper

--- vagrant/database/database_setup.py
+++ vagrant/database/database_setup.py
@@ -5,6 +5,22 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 base = declarative_base()
+
+
+class Restaurant (Base):
+    __tablename__ = 'restaurant'
+    name = Column(
+        string(80), nullable=False
+    )
+    id = Column(
+        integer, primary_key=True
+    )
+
+
+class MenuItem (Base):
+    __tablename__ = 'menu_item'
+
+
 ####### insert at end of file #######
 
 engine = create_engine(
