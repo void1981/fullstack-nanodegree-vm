b881ef212c530257bc6f0aa2c42e6d75a8d012a8
void1981 <void1950@gmail.com>
2019-04-26 18:28:03 +0200

    create Mapper and attributes in classes

--- vagrant/database/database_setup.py
+++ vagrant/database/database_setup.py
@@ -1,29 +1,44 @@
 import sys
-from sqlalchemy import Column, ForeignKey, Interger, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-base = declarative_base()
+Base = declarative_base()
 
 
 class Restaurant (Base):
     __tablename__ = 'restaurant'
     name = Column(
-        string(80), nullable=False
+        String(80), nullable=False
     )
     id = Column(
-        integer, primary_key=True
+        Integer, primary_key=True
     )
 
 
 class MenuItem (Base):
     __tablename__ = 'menu_item'
+    name = Column(
+        String(80), nullable=False
+    )
+    id = Column(Integer, primary_key=True)
+
+    course = Column(String(250))
+
+    description = Column(String(250))
+
+    price = Column(String(8))
+
+    restaurant_id = Column(
+        Integer, ForeignKey('restaurant.id')
+    )
+
+    restaurant = relationship(Restaurant)
 
 
 ####### insert at end of file #######
-
 engine = create_engine(
     'sqlite:///restaurantmenu.db')
 
-base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
