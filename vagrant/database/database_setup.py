a669707c4b1ed4f74da96fc147f1732080b35275
void1981 <void1950@gmail.com>
2019-04-26 17:52:19 +0200

    create configuration code for SQL Alchemy

---
+++ vagrant/database/database_setup.py
@@ -0,0 +1,13 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Interger, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+base = declarative_base()
+####### insert at end of file #######
+
+engine = create_engine(
+    'sqlite:///restaurantmenu.db')
+
+base.metadata.create_all(engine)
